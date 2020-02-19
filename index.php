20372ed2e9f5feef7b50039d97ea36877479a5a7
Swapnil Jain <swapnil@linux.com>
2020-02-19 14:43:58 +1100

    updated index.php from friends

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
+$servername = "mysql";
+$username = getenv(MYSQL_USER);
+$password = getenv(MYSQL_PASSWORD);
+$dbname = getenv(MYSQL_DATABASE);
 
-print "<h1>Welcome to ".$_SERVER['SERVER_ADDR']."</h1>";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
