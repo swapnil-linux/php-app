8cc735221b028c0e918bd4ca3589a716a26662bb
Swapnil Jain <swapnil@linux.com>
2020-12-14 14:17:02 +1100

    fixed a newline

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if ($result->num_rows > 0) {
         echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
     }
 } else {
-    echo "0 results";
+    echo "<p>0 results</p>";
 }
 $conn->close();
 ?>
