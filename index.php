086390a44ff6fdb0ec4b731c669c6946c3bc5685
Swapnil Jain <swapnil@linux.com>
2020-12-15 22:56:22 +1100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,8 @@ if ($conn->connect_error) {
 
 $sql = "SELECT id, firstname, lastname FROM MyGuests";
 $result = $conn->query($sql);
-print "List of Friends from " . $_SERVER['SERVER_ADDR'];
+print "<h1>List of Friends from " . $_SERVER['SERVER_ADDR'];
+print "<h1><br><br>";
 
 if ($result->num_rows > 0) {
     // output data of each row
