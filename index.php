47f96622ba6163af6260593a1b416f354a7d4d0d
Swapnil Jain <swapnil@linux.com>
2022-06-27 09:50:06 +1000

    some improvements

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ if ($conn->connect_error) {
 
 $sql = "SELECT id, firstname, lastname FROM MyGuests";
 $result = $conn->query($sql);
+print "<img src='friends.jpg' />";
 print "<h1>List of Friends from " . $_SERVER['SERVER_ADDR'];
 print "<h1><br><br>";
 
